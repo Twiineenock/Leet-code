3d04a8de6eb55d4321e25d88cef71c3a2fb745d6
Twiine Enock <twiineenockfox@gmail.com>
2025-08-13 23:34:04 +0300

    start on the palindrome number problem

---
+++ JavaProblems/src/PalindromeNumber.java
@@ -0,0 +1,10 @@
+/*
+* {@link https://leetcode.com/problems/palindrome-number/ }
+* */
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int number) {
+        //code goes here
+        return true;
+    }
+}
